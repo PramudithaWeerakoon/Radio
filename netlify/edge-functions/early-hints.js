e125a107227a4d58e4561aa5cc9792946eceec01
PramudithaWeerakoon <tushan.janith@gmail.com>
2025-04-20 12:22:15 +0530

    fix: update edge function for early hints to improve resource preloading and error handling

--- netlify/edge-functions/early-hints.js
+++ netlify/edge-functions/early-hints.js
@@ -1,34 +1,37 @@
 // Early Hints Edge Function for Netlify
 // This helps with preloading critical resources
 
-export default async function earlyHints(request, context) {
-  // List of critical resources to preload
-  const criticalResources = [
-    { path: "/_next/static/css/1773a1dda5c4e148.css", as: "style" },
-    { path: "/_next/static/chunks/main.js", as: "script" },
-    { path: "/_next/static/chunks/pages/_app.js", as: "script" },
-    { path: "/_next/static/chunks/webpack.js", as: "script" },
-    { path: "/_next/static/chunks/framework.js", as: "script" },
-  ];
+export default async function onRequest(context) {
+  try {
+    // List of critical resources to preload
+    const criticalResources = [
+      { path: "/_next/static/css/1773a1dda5c4e148.css", as: "style" },
+      { path: "/_next/static/chunks/main.js", as: "script" },
+      { path: "/_next/static/chunks/pages/_app.js", as: "script" },
+      { path: "/_next/static/chunks/webpack.js", as: "script" },
+      { path: "/_next/static/chunks/framework.js", as: "script" },
+    ];
 
-  // Generating 103 Early Hints headers
-  const earlyHintsHeaders = criticalResources.map(
-    (resource) => `Link: <${resource.path}>; rel=preload; as=${resource.as}`
-  );
+    // Generate Link headers for preloading
+    const linkHeaders = criticalResources.map(
+      (resource) => `<${resource.path}>; rel=preload; as=${resource.as}`
+    );
+    
+    // Add preconnect hints for external resources
+    linkHeaders.push('<https://images.unsplash.com>; rel=preconnect');
+    linkHeaders.push('<https://fonts.googleapis.com>; rel=preconnect');
+    linkHeaders.push('<https://fonts.gstatic.com>; rel=preconnect; crossorigin');
 
-  // Send early hints
-  context.log(`Sending Early Hints for request: ${request.url}`);
-  
-  // Add preconnect hints for external resources
-  earlyHintsHeaders.push('Link: <https://images.unsplash.com>; rel=preconnect');
-  earlyHintsHeaders.push('Link: <https://fonts.googleapis.com>; rel=preconnect');
-  earlyHintsHeaders.push('Link: <https://fonts.gstatic.com>; rel=preconnect; crossorigin');
-
-  // Return response with proper HTTP 103 Early Hints
-  context.sendEarlyHints({
-    headers: { Link: earlyHintsHeaders.join(', ') },
-  });
-  
-  // Pass through the request to the origin
-  return context.next();
+    // Get the response from the origin
+    const response = await context.next();
+    
+    // Add the Link header to the response
+    response.headers.set('Link', linkHeaders.join(', '));
+    
+    // Return the modified response
+    return response;
+  } catch (error) {
+    console.error('Edge function error:', error);
+    return context.next();
+  }
 }
